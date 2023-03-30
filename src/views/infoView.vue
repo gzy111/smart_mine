5e87e866909e1129639513438db1beb98d47bd85
zyguan <ziyuan.guan@eisgroup.com>
2023-03-30 18:05:28 +0800

    3.30 文件上传

--- src/views/infoView.vue
+++ src/views/infoView.vue
@@ -1,90 +1,66 @@
 <template>
-  <el-upload
-  :action="uploadUrl"
-  :on-success="handleUploadSuccess"
-  :before-upload="beforeUpload2"
-  :multiple="false"
-  :file-list="fileList"
-  accept=".pdf,.doc,.docx,.xls,.xlsx"
-  show-file-list
-  list-type="picture-card"
->
-  <i class="el-icon-plus"></i>
-  <div class="upload-text">上传文件</div>
-</el-upload>
 
-<template v-for="item in toRefs(dlist)" :key="item">
-  <li><a :href="item.value.documentUrl">{{ item.value.id }}</a></li>
-</template>
-<a href="http://localhost:80/File/download?name=/2023-03-28/附件1.河池市2023年事业单位公开招聘工作人员岗位表2023.3.15.xls">下载</a>
-</template>
+<div class="big-card">
 
+
+<div class="card">
+  <img src="../style/image/exce.png" alt="">
+</div>
+<div class="card-title">
+  <span
+ title="ss" 
+ class="branch_name_front
+ ">
+ CSS 测试标题，这是一个稍微有点长的标题，超出一行以后才会有title提示，标题是 实现优惠券的技巧 - 2021-03-26
+ </span>
+
+</div>
+
+</div>
+</template>
 <script lang="ts" setup >
-import { computed, ref, reactive, toRaw, getCurrentInstance ,toRefs} from 'vue'
-import{DocumentListAPI,DocumentDownAPI} from '../api/upload'
-const uploadUrl = ref(import.meta.env.VITE_APP_BASE_API + '/File/UploadImage')
-
-const fileUrl=ref('')
-const { proxy } = getCurrentInstance()
-
-const handleUploadSuccess = (response) => {
-  // 上传成功后的处理
-  proxy.$message.success('上传成功')
-  console.log(response);
-
-}
-
-const beforeUpload2 = (file: File) => {
-  const fileTypes = ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']
-  const fileType = file.type
-  const isLt2M = file.size / 1024 / 1024 < 10
-  console.log(file);
-  
-  if (!fileTypes.includes(fileType)) {
-    proxy.$message.error('上传文件只能是 PDF、Word 或 Excel 格式!')
-  }
-  if (!isLt2M) {
-    proxy.$message.error('上传文件大小不能超过 10MB!')
-  }
-  return fileTypes.includes(fileType) && isLt2M
-}
-const handleUploadSuccess2 = (response) => {
-  // 上传成功后的处理
-  proxy.$message.success('上传成功')
-  console.log(response);
-  fileUrl.value = response
-}
-const dlist=ref([])
-const info=ref([])
-function documentList() {
-  DocumentListAPI({}).then((res: any) => {
-    for (const key in res.data) {
-        let re=reactive({
-            id:'',
-            label:''
-        })
-        re.id=res.data[key].deptId
-        re.label=res.data[key].deptName
-        info.value.push(re)
-    }
-
-  });
-}
-documentList()
-console.log(dlist);
-
-function documentDown() {
-  DocumentDownAPI({name:"房屋出租.jpg",isOnline:0}).then((res: any) => {
-    console.log(res);
-  });
-}
 
 </script>
 <style>
-.avatar {
-  width: 178px;
-  height: 178px;
-  display: block;
-  background-color: antiquewhite;
+
+.card{
+  padding: 0;
+  height: 130px;
+  width: 130px;
+  background-color: rgb(232, 232, 233);
+  border-radius: 5px 5px ;
 }
+.card img{
+  height: 100%;
+  width: 100%;
+}
+.big-card{
+  position: relative;
+  height: 160px;
+  width: 130px;
+  background-color: rgb(114, 93, 52);
+}
+.card-title{
+  font-size:14px;
+  background-color: aqua;
+  width: 130px;
+}
+
+.branch_name_front {
+    display: block;
+    text-align: justify;
+    overflow: hidden;
+    height: 20px;
+    white-space: normal;
+}
+.branch_name_front::before{
+    content: attr(title);
+    width: 60%;
+    float: right;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    direction: rtl;
+}
+
 </style>
